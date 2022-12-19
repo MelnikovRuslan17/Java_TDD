62bc368ceb8944a419122c8f0700e57bed961587
Melnikov Ruslan <Melnikovruslan17@yandex.ru>
2022-12-19 21:52:39 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> contacts = new HashMap<>();
+
+    public int addContact(String name, String phoneNumber){
+        return null;
 
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,33 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBookTest {
+    PhoneBook phoneBook;
+
+
+    @BeforeEach
+    public void initPhoneBook() {
+        phoneBook = new PhoneBook();
+    }
+    @AfterEach
+    public void nullPhonebook(){
+        phoneBook = null;
+
+    }
+    @Test
+    public void testAddContact(){
+        String name = "Екатерина";
+        String phoneNumber = "9-999-999-99";
+
+        int expected = 1;
+        int result = phoneBook.addContact(name, phoneNumber);
+
+        assert(expected, result);
+    }
+
+
 }
+
