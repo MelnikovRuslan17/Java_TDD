169c640515d3984b27d98f53e99fe47c2a383ce9
Melnikov Ruslan <Melnikovruslan17@yandex.ru>
2022-12-19 22:23:38 +0300

    Stub and tests fo findByNumber

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -14,22 +14,36 @@ public class PhoneBookTest {
     public void initPhoneBook() {
         phoneBook = new PhoneBook();
     }
+
     @AfterEach
-    public void nullPhonebook(){
+    public void nullPhonebook() {
         phoneBook = null;
 
     }
+
     @Test
-    public void testAddContact(){
+    public void testAddContact() {
         String name = "Екатерина";
         String phoneNumber = "9-999-999-99";
 
         int expected = 1;
         int result = phoneBook.addContact(name, phoneNumber);
 
-        assertEquals (expected, result);
+        assertEquals(expected, result);
     }
 
+    @Test
+    public void testFindByNumber(){
+        phoneBook.addContact("Анна", "8-888-888-77");
+        phoneBook.addContact("Ирина", "8-888-999-99");
+        phoneBook.addContact("Олег", "8-888-000-07");
+
+        String phoneNumber = "8-888-00-07";
+        String expected = "Олег";
+        String result = phoneBook.findByNumber(phoneNumber);
+        assertEquals(expected,result);
+
+    }
 
 }
 
